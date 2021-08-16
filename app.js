4d0e687d6b0163e204175339d345089cbe50776b
Aakanshalog <akanksha.products@yahoo.com>
2021-08-16 07:23:10 +0530

    all ok

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.use(cors());
 app.use("/api",postsrouter);
 
 
-const port =  process.env.port || 7000;
+const port =  process.env.PORT || 7000;
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
